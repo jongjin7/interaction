66977e086b352497be5d6d53ee59962f1124ae13
jongjin7 <ezcode2875@gmail.com>
2024-08-25 11:26:50 +0900

    :modify [핀 갤러리 Next]  - 상세 화면 활성/비활성 기능 추가

--- src/pin-gallery-next/src/app/_list/ListPage.tsx
+++ src/pin-gallery-next/src/app/_list/ListPage.tsx
@@ -1,19 +1,42 @@
 'use client';
 
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useRef, useEffect } from 'react';
 import ListDetail from '@/app/_list/ListDetail';
 import { ShowDetailContext } from '@/app/_data/ShowDetailProvider';
 import Tabs from './tab-nav/Tabs';
 import TabContent from './tab-content/TabContent';
 
 const ListPage: React.FC = () => {
+  const activeShowClass = 'show-detail';
+  const pageRef = useRef<HTMLElement | null>(null);
   const { showDetail } = useContext(ShowDetailContext);
   const [currentTabIndex, setCurrentTabIndex] = useState<number>(0);
+
+  useEffect(() => {
+    if (pageRef.current) {
+      const parentElement = pageRef.current.parentElement as HTMLElement | null;
+
+      if (parentElement) {
+        const pageFrame = parentElement;
+
+        if (showDetail) {
+          pageFrame.classList.add(activeShowClass);
+        } else {
+          pageFrame.classList.remove(activeShowClass);
+        }
+      } else {
+        console.warn('Parent element is null.');
+      }
+    } else {
+      console.warn('pageRef.current is null.');
+    }
+  }, [showDetail]);
+
   return (
     <>
-      <div className="page-container">
+      <div ref={pageRef} className="page-container">
         <Tabs tabControl={{ currentTabIndex, setCurrentTabIndex }} />
-        <TabContent tabControl={{ currentTabIndex, setCurrentTabIndex }} setShowDetail />
+        <TabContent tabControl={{ currentTabIndex, setCurrentTabIndex }} />
       </div>
       {showDetail && <ListDetail imageSrc={showDetail} />}
     </>
