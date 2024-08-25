657ad606df2206c7c0b394bd146476574d7b695d
jongjin7 <ezcode2875@gmail.com>
2024-08-25 22:16:44 +0900

    :modify [핀 갤러리 Next] - 패널 타이이틀 컴포넌트 수정

--- src/pin-gallery-next/src/app/_list/tab-content/TabContentListTitle.tsx
+++ src/pin-gallery-next/src/app/_list/tab-content/TabContentListTitle.tsx
@@ -9,20 +9,18 @@ interface ListTitleProps {
 const TabContentListTitle: React.FC<ListTitleProps> = ({ ...props }) => {
   const { title, subTitle, itemLength } = props;
   return (
-    <div className="list-header">
-      <div className="title">
-        <h2 className="font-semibold">
-          {title} {!subTitle && itemLength ? `(${itemLength})` : ''}
-        </h2>
-        {subTitle ? (
-          <small className="text-gray-500">
-            {subTitle}
-            {` (${itemLength})`}
-          </small>
-        ) : (
-          ''
-        )}
-      </div>
+    <div className="title">
+      <h2 className="font-semibold">
+        {title} {!subTitle && itemLength ? `(${itemLength})` : ''}
+      </h2>
+      {subTitle ? (
+        <small className="text-gray-500">
+          {subTitle}
+          {` (${itemLength})`}
+        </small>
+      ) : (
+        ''
+      )}
     </div>
   );
 };
