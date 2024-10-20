2b2c5a164dd7655c2928f509b46386007f37af91
jongjin7 <ezcode2875@gmail.com>
2024-10-20 14:45:49 +0900

    :refactor [핀 갤러리 Next-Justand] - viewport 재설정(아이폰 input:focusin에서 화면 확대되는 문제)

--- framework_dev/pin-gallery-next-zustand/src/app/layout.tsx
+++ framework_dev/pin-gallery-next-zustand/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import React from 'react';
 import '@/styles/default.scss';
 import '@/styles/main.css';
@@ -6,6 +6,15 @@ import '@/styles/main.css';
 export const metadata: Metadata = {
   title: 'Pin-Gallery by Next.js & Zustand',
   description: 'Next.js와 Zustand로 포팅한 갤러리',
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  // Also supported by less commonly used
+  // interactiveWidget: 'resizes-visual',
 };
 
 export default function RootLayout({
