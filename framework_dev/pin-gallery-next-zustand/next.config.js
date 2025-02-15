6b843227d75e44ade245d76440c2830d80e0e826
jongjin7 <ezcode2875@gmail.com>
2025-02-15 10:55:13 +0900

    :refactor [핀 갤러리 Next-Justand] - 프로젝트 외부 경로 참조하기위한 설정 변경

--- framework_dev/pin-gallery-next-zustand/next.config.js
+++ framework_dev/pin-gallery-next-zustand/next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 
 const withVanillaExtract = createVanillaExtractPlugin();
@@ -16,12 +18,19 @@ const nextConfig = {
       use: ['@svgr/webpack'],
     });
 
+    // ✅ Webpack alias 설정 (프로젝트 외부 폴더 접근 가능하도록)
+    config.resolve.alias['@client-services'] = path.resolve(__dirname, '../../client-services');
+
+    config.resolve.modules = [
+      path.resolve(__dirname, 'src'),
+      path.resolve(__dirname, '../client-services'),
+      'node_modules',
+    ];
     return config;
   },
   typescript: {
     ignoreBuildErrors: true, // TypeScript 오류를 빌드 중 무시합니다.
   },
-  // transpilePackages: ['@company/design-system'],
 };
 
 module.exports = withVanillaExtract(nextConfig);
